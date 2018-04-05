c65db09855939f86d821d12ec32142c6d74abe13
kacperfonder <kacper.fonder@gmail.com>
2018-04-05 23:54:16 +0200

    button disable/enable

--- js/app.js
+++ js/app.js
@@ -6,7 +6,8 @@ const cart = {
     myP: document.querySelectorAll('#gameName'),
     myBtns: document.querySelectorAll('.gameBtn'),
     price: document.querySelector('.price'),
- 
+    totalPrice: 0,
+   
     
     generateBtns () {
         for (let i=0; i<this.myBtns.length; i++ ){
@@ -16,79 +17,93 @@ const cart = {
             this.myP[i].innerHTML = this.myLi[i].dataset.name;
         } 
     },
-    removeBtn () {
-        const removeBtn = document.querySelectorAll('.remove');
-        removeBtn.forEach(element => {
-            element.addEventListener('click', () => {
-                const ele = element.closest('li');
-                ele.remove()
-                this.totalPrice();
-                this.buttonON()
-            })
-        })
-    },
-    buttonON () {
-        const buttons = this.elemGameList.querySelectorAll('button');
-        buttons.forEach(e => {
-            if(e.disabled = true) {
-                e.disabled = false;
-            }
-        })
-    },
-   
-    clearCartBtn () {
-        this.elemCart.querySelector('button').addEventListener("click", () => {
-            const clearAll = this.elemCart.querySelectorAll('li')
-            this.price.innerText = ''
-            clearAll.forEach(element => {
-                element.remove();
-            })
-            this.buttonON();
-        })
-    },
-    totalPrice () {
-        const cartItem = this.elemCart.querySelectorAll('p')
-        let sum = 0;
-     
-        for (let i= 0; i<cartItem.length; i++) {       
-            sum += parseFloat(cartItem[i].innerHTML);
-            this.price.innerHTML = '$ ' + sum;
-        }
-    },
-    
+
     btnsOnClick () { 
         this.myBtns.forEach(element => {
             element.addEventListener('click', () => {
-             
+            
                 const close = element.closest('li');
              
                 const cartCreate = document.createElement('li');
+                const spanCreate = document.createElement('span')
                 const imgCreate = document.createElement('img');
-                const cartPrice = document.createElement('p');
                 const removeBtn = document.createElement('button');
+                const pCreate = document.createElement('p');
+                spanCreate.classList.add('spanPrice')
+                removeBtn.classList.add('remove');
+                removeBtn.innerText = 'remove';
+                removeBtn.dataset.id = element.dataset.id
+                removeBtn.dataset.price = element.dataset.price
+
 
                 const cartLi = cartCreate;
-                
-                cartPrice.innerText =  close.dataset.price
-                cartCreate.innerText = close.dataset.name;
-                removeBtn.innerText = 'remove'
-                removeBtn.classList.add('remove');
+                pCreate.innerText =  '$ '
+                spanCreate.innerText = close.dataset.price
                 imgCreate.src = close.dataset.src
+                console.log(removeBtn.dataset.id);
                 
                 this.elemCart.appendChild(cartCreate)
+                pCreate.appendChild(spanCreate)
                 cartLi.appendChild(removeBtn)
                 cartLi.appendChild(imgCreate)
-                cartLi.appendChild(cartPrice) 
+                cartLi.appendChild(pCreate) 
                 element.disabled = true;
                 if(element.disabled = true) {
                     element.innerText = 'IN CART';
                 }
                 this.removeBtn();
                 this.totalPrice();
-                console.log(cartPrice);
             });
         });  
     },
+
+    removeBtn () {
+        const removeBtn = document.querySelectorAll('.remove');
+        removeBtn.forEach(element => {     
+          
+            element.addEventListener('click', () => {      
+                const ele = element.closest('li');
+                ele.remove()
+                let id = document.querySelector('[data-id='+element.dataset.id+']')
+                let price = document.querySelector('[data-price='+element.dataset.price+']')
+                id.removeAttribute("disabled");
+               
+                console.log(element.dataset.price);
+                id.innerText = price;
+                this.totalPrice();
+
+            })
+        })
+    },
+
+    totalPrice () {
+        const cartItem = this.elemCart.querySelectorAll('.spanPrice')
+        sum = 0;
+        for (let i=0; i<cartItem.length; i++) {  
+            sum += parseFloat(cartItem[i].innerHTML);
+            this.price.innerHTML = '$ ' + sum.toFixed(2)
+        }
+    },
+
+    buttonON () {
+        const btns = document.querySelectorAll('.gameBtn')
+        btns.forEach(element => {
+            
+        })
+    },
+             
+    clearCartBtn () {
+        this.elemCart.querySelector('button').addEventListener("click", () => {
+            const clearAll = this.elemCart.querySelectorAll('li')
+            this.price.innerText = '';
+            // this.buttonON();
+            clearAll.forEach(element => {
+                element.remove();
+            })
+            this.totalPrice();
+        })
+    },
+
     shoppingCart() {
         this.generateBtns();
         this.btnsOnClick();
