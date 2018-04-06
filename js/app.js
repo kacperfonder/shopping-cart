0d8a8ba81a3c1d77b927a4922f12946d09857e2e
kacperfonder <kacper.fonder@gmail.com>
2018-04-06 23:58:01 +0200

    add css

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ const cart = {
     myBtns: document.querySelectorAll('.gameBtn'),
     price: document.querySelector('.price'),
     totalPrice: 0,
+    cartInfo: document.querySelector('.cartInfo'),
+    sum: 0,
    
-    
     generateBtns () {
         for (let i=0; i<this.myBtns.length; i++ ){
             this.myBtns[i].innerHTML = '$ ' + this.myLi[i].dataset.price;
@@ -28,80 +29,90 @@ const cart = {
                 const spanCreate = document.createElement('span')
                 const imgCreate = document.createElement('img');
                 const removeBtn = document.createElement('button');
+                const div = document.createElement('div')
                 const pCreate = document.createElement('p');
                 spanCreate.classList.add('spanPrice')
                 removeBtn.classList.add('remove');
                 removeBtn.innerText = 'remove';
                 removeBtn.dataset.id = element.dataset.id
-                removeBtn.dataset.price = element.dataset.price
-
-
-                const cartLi = cartCreate;
-                pCreate.innerText =  '$ '
-                spanCreate.innerText = close.dataset.price
-                imgCreate.src = close.dataset.src
-                console.log(removeBtn.dataset.id);
+                removeBtn.dataset.btnprice = element.dataset.btnprice
                 
-                this.elemCart.appendChild(cartCreate)
-                pCreate.appendChild(spanCreate)
-                cartLi.appendChild(removeBtn)
-                cartLi.appendChild(imgCreate)
-                cartLi.appendChild(pCreate) 
+                pCreate.innerText =  close.dataset.name
+                spanCreate.innerText = "$ "+ close.dataset.price
+                spanCreate.dataset.price = close.dataset.price
+                imgCreate.src = close.dataset.src
+                
+                this.elemCart.appendChild(cartCreate);
+                cartCreate.appendChild(imgCreate);
+                cartCreate.appendChild(div);
+                cartCreate.appendChild(spanCreate);
+                div.appendChild(pCreate);
+                div.appendChild(removeBtn);
                 element.disabled = true;
                 if(element.disabled = true) {
                     element.innerText = 'IN CART';
                 }
+                this.counter ++;
                 this.removeBtn();
                 this.totalPrice();
             });
         });  
     },
-
     removeBtn () {
         const removeBtn = document.querySelectorAll('.remove');
         removeBtn.forEach(element => {     
           
             element.addEventListener('click', () => {      
+                // this.counter --;
                 const ele = element.closest('li');
                 ele.remove()
                 let id = document.querySelector('[data-id='+element.dataset.id+']')
-                let price = document.querySelector('[data-price='+element.dataset.price+']')
                 id.removeAttribute("disabled");
-               
-                console.log(element.dataset.price);
-                id.innerText = price;
+                id.innerText = "$ "+ element.dataset.btnprice;
                 this.totalPrice();
-
             })
         })
     },
 
     totalPrice () {
-        const cartItem = this.elemCart.querySelectorAll('.spanPrice')
-        sum = 0;
-        for (let i=0; i<cartItem.length; i++) {  
-            sum += parseFloat(cartItem[i].innerHTML);
-            this.price.innerHTML = '$ ' + sum.toFixed(2)
-        }
-    },
-
-    buttonON () {
-        const btns = document.querySelectorAll('.gameBtn')
-        btns.forEach(element => {
+        const cartItem = this.elemCart.querySelectorAll('.spanPrice');
+      
+        let inCart = document.querySelector('.inCart');
+        counter = 0;
+        for (let i=0; i<=cartItem.length; i++) {  
             
-        })
-    },
-             
+            let counter = document.querySelector('.counter');
+
+            if(this.sum === 0 ){
+                this.sum = 0.00;
+            } else {
+                this.sum += parseFloat(cartItem[i].dataset.price);
+            }
+            
+            this.price.innerHTML = '$ ' + sum.toFixed(2);
+
+            counter.innerHTML = cartItem.length;
+            inCart.innerHTML = cartItem.length + ' ITEMS IN CART';
+        }
+    },          
     clearCartBtn () {
-        this.elemCart.querySelector('button').addEventListener("click", () => {
-            const clearAll = this.elemCart.querySelectorAll('li')
+        this.cartInfo.querySelector('button').addEventListener("click", () => {
+            const clearAll = this.elemCart.querySelectorAll('li');
             this.price.innerText = '';
-            // this.buttonON();
+            this.counter = 0;
             clearAll.forEach(element => {
-                element.remove();
+                element.remove(); 
             })
+            let buttons = this.elemGameList.querySelectorAll('button');  
+            buttons.forEach(e => {
+               e.innerText = "$ "+ e.dataset.btnprice
+                if(e.disabled = true) {
+                    e.disabled = false;
+                };
+            });
+        
             this.totalPrice();
-        })
+        });
     },
 
     shoppingCart() {
